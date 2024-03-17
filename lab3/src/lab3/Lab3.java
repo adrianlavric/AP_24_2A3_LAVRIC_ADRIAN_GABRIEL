1dc1c8d55af8df41863fd293dfe3cb607d64c13d
adrianlavric <115322394+adrianlavric@users.noreply.github.com>
2024-03-17 15:24:41 +0200

    Update Lab3.java

--- lab3/src/lab3/Lab3.java
+++ lab3/src/lab3/Lab3.java
@@ -38,14 +38,35 @@ public class Lab3 {
         trip.addAttraction(church1);
         trip.addAttraction(concert1);
 
+        System.out.println("Compulsory: ");
         System.out.println(trip);
         System.out.println(church1.getTimetable());
         System.out.println(concert1.getTicketPrice());
     }
 
     public void Homework() {
+        Trip trip = new Trip("Iasi", LocalDate.of(2024, 7, 1), LocalDate.of(2024, 7, 8));
 
+        Statue statue1 = new Statue("Statue", "statue");
+        Church church1 = new Church("Church", "church");
+        Concert concert1 = new Concert("Concert", "concert", 100.0);
 
+        Map<LocalDate, TimeInterval> churchTimetable = new HashMap<>();
+        churchTimetable.put(LocalDate.of(2024, 7, 1), new TimeInterval(LocalTime.of(6, 0), LocalTime.of(8, 0)));
+        church1.setTimetable(churchTimetable);
+
+        Map<LocalDate, TimeInterval> concertTimetable = new HashMap<>();
+        concertTimetable.put(LocalDate.of(2024, 7, 2), new TimeInterval(LocalTime.of(7, 0), LocalTime.of(22, 0)));
+        concert1.setTimetable(concertTimetable);
+
+        trip.addAttraction(statue1);
+        trip.addAttraction(church1);
+        trip.addAttraction(concert1);
+
+        TravelPlan travelPlan = new TravelPlan(trip);
+
+        System.out.println("Homework: ");
+        System.out.println(travelPlan);
 
     }
 }
